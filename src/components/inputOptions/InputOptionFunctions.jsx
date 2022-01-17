110e1cbc1ff951a0b2e6ff151433df7615c93ecf
Jefri Lunn <jefri.haigh.lunn@gmail.com>
2022-01-17 15:48:03 +0000

    added functionality for other inputs to be added to form

--- src/components/inputOptions/InputOptionFunctions.jsx
+++ src/components/inputOptions/InputOptionFunctions.jsx
@@ -143,85 +143,169 @@ const addButtonField = () => {
 }
 
 const addCheckboxField = () => {
-    
+    const obj = {
+        type:"checkbox",
+        value:""
+    }
+    return obj
 }
 
 const addColorField = () => {
-
+    const obj = {
+        type:"color",
+        value:""
+    }
+    return obj
 }
 
 const addDateField = () => {
-
+    const obj = {
+        type:"date",
+        value:""
+    }
+    return obj
 }
 
 const addDateTimeLocalField = () => {
-
+    const obj = {
+        type:"datetime-local",
+        value:""
+    }
+    return obj
 }
 
 const addEmailField = () => {
-
+    const obj = {
+        type:"email",
+        value:""
+    }
+    return obj
 }
 
 const addFileField = () => {
-
+    const obj = {
+        type:"file",
+        value:""
+    }
+    return obj
 }
 
 const addHiddenField = () => {
-
+    const obj = {
+        type:"hidden",
+        value:""
+    }
+    return obj
 }
 
 const addImageField = () => {
-
+    const obj = {
+        type:"image",
+        value:""
+    }
+    return obj
 }
 
 const addMonthField = () => {
-
+    const obj = {
+        type:"month",
+        value:""
+    }
+    return obj
 }
 
 const addNumberField = () => {
-
+    const obj = {
+        type:"number",
+        value:""
+    }
+    return obj
 }
 
 const addPasswordField = () => {
-
+    const obj = {
+        type:"password",
+        value:""
+    }
+    return obj
 }
 
 const addRadioField = () => {
-
+    const obj = {
+        type:"radio",
+        value:""
+    }
+    return obj
 }
 
 const addRangeField = () => {
-
+    const obj = {
+        type:"range",
+        value:""
+    }
+    return obj
 }
 
 const addResetField = () => {
-    
+    const obj = {
+        type:"reset",
+        value:""
+    }
+    return obj
 }
 
 const addSearchField = () => {
-
+    const obj = {
+        type:"search",
+        value:""
+    }
+    return obj
 }
 
 const addSubmitField = () => {
-
+    const obj = {
+        type:"submit",
+        value:""
+    }
+    return obj
 }
 
 const addTelField = () => {
-
+    const obj = {
+        type:"tel",
+        value:""
+    }
+    return obj
 }
 
 const addTextField = () => {
-
+    const obj = {
+        type:"text",
+        value:""
+    }
+    return obj
 }
 
 const addTimeField = () => {
-
+    const obj = {
+        type:"time",
+        value:""
+    }
+    return obj
 }
 
 const addURLField = () => {
-
+    const obj = {
+        type:"url",
+        value:""
+    }
+    return obj
 }
 
 const addWeekField = () => {
-
+    const obj = {
+        type:"week",
+        value:""
+    }
+    return obj
 }
